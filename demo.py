03b01687fc7bbf1694dd5e23c67e9bd618d2be82
Jesperoka <jesperoka@gmail.com>
2024-01-27 00:05:13 +0100

    added panda and driving surface to scene

--- demo.py
+++ demo.py
@@ -15,24 +15,22 @@ SCENE = "./mujoco_models/scene.xml"
 
 
 @jax.jit
-def ctrl_from_action(v: float, theta: float, omega) -> jnp.ndarray:
-    return jnp.array([v*jax.lax.cos(theta), v*jax.lax.sin(theta), omega])
+def ctrl_from_action(v: float, theta: float, omega, joint_angles: list) -> jnp.ndarray:
+    return jnp.array([*joint_angles, v*jax.lax.cos(theta), v*jax.lax.sin(theta), omega])
 
-# @jax.jit
-def step(mjx_model: mjx.Model, mjx_data: mjx.Data, v: float, theta: float, omega: float):
-    print(mjx_data.ctrl)
-    mjx_data = mjx_data.replace(ctrl=ctrl_from_action(v, theta, omega))
-    print(mjx_data.ctrl)
-    input()
+@jax.jit
+def step(mjx_model: mjx.Model, mjx_data: mjx.Data, v: float, theta: float, omega: float, joint_angles: jnp.ndarray):
+    mjx_data = mjx_data.replace(ctrl=ctrl_from_action(v, theta, omega, joint_angles))
     mjx_data = mjx.step(mjx_model, mjx_data)
     return mjx_data
 
+# TODO: vmap and parallelize environment as done in the brax tutorial colab
 if __name__ == "__main__":
     model = mj.MjModel.from_xml_path(SCENE)
     data = mj.MjData(model)
     renderer = mujoco.Renderer(model)
 
-    viewer = mj.viewer.launch(model, data)
+    # viewer = mj.viewer.launch(model, data)
 
     mj.mj_resetData(model, data)
     mjx_model = mjx.put_model(model)
@@ -40,7 +38,8 @@ if __name__ == "__main__":
 
     print("local_devices: ", jax.local_devices())
     print("jitting step function")
-    mjx_data = step(mjx_model, mjx_data, 0.0, 0.0, 0.0)
+    joint_angles=[0.0, -0.688, 0, -1.78, 0, 1.09, -2.32]
+    mjx_data = step(mjx_model, mjx_data, 0.0, 0.0, 0.0, joint_angles)
     print("done jitting")
 
     frames = []
@@ -57,7 +56,7 @@ if __name__ == "__main__":
         theta = jax.lax.sin(2*mjx_data.time)
         omega = 0.2
 
-        mjx_data = step(mjx_model, mjx_data, v, theta, omega) 
+        mjx_data = step(mjx_model, mjx_data, v, theta, omega, joint_angles) 
         if i < mjx_data.time * fps:
             data = mjx.get_data(model, mjx_data)
             renderer.update_scene(data)
