10d538ab0e2716f8f134118dab7ac3ff1a00f2fd
Jesperoka <jesperoka@gmail.com>
2024-02-04 17:41:57 +0100

    started remaking the official code into what I need

---
+++ zeus_car/car_control.cpp
@@ -0,0 +1,62 @@
+#include "car_control.h"
+
+#include <Arduino.h>
+#include <SoftPWM.h>
+
+#define MOTOR_POWER_MIN 28
+#define MOTOR_POWER_MAX 255
+#define MOTOR_START_POWER 100
+
+
+int32_t _lastError = 0;
+int32_t errorIntegral = 0;
+int16_t originHeading;
+
+/** @brief Set speed for 4 motors
+ * @param powerX  0 ~ 100 */
+void set_motors(int8_t power0, int8_t power1, int8_t power2, int8_t power3) {
+    bool dir[4];
+    int8_t power[4] = {power0, power1, power2, power3};
+    int8_t newPower[4];
+
+    for (uint8_t i = 0; i < 4; i++) {
+        dir[i] = power[i] > 0;
+
+        if (MOTOR_DIRECTIONS[i])
+            dir[i] = !dir[i];
+
+        if (power[i] == 0) {
+            newPower[i] = 0;
+        } else {
+            newPower[i] = map(abs(power[i]), 0, 100, MOTOR_POWER_MIN, 255);
+        }
+
+        if (newPower[i] != 0 && newPower[i] < MOTOR_START_POWER) {
+            SoftPWMSet(MOTOR_PINS[i * 2], dir[i] * MOTOR_START_POWER);
+            SoftPWMSet(MOTOR_PINS[i * 2 + 1], !dir[i] * MOTOR_START_POWER);
+            delayMicroseconds(200);
+        }
+        SoftPWMSet(MOTOR_PINS[i * 2], dir[i] * newPower[i]);
+        SoftPWMSet(MOTOR_PINS[i * 2 + 1], !dir[i] * newPower[i]);
+    }
+}
+
+/** @param angle the direction you want the car to move
+ * @param power 0 ~ 100 (percentage)
+ * @param rot the car fixed rotation angle during the movement */
+void move(int16_t angle, int8_t power, int8_t rot, bool drift) {
+    angle += 90; // 0 is forward
+    float rad = angle * PI / 180;
+
+    float ratio = 0.5;
+
+    power /= sqrt(2);
+    power = power * (1 - ratio);
+
+    int8_t power_0 = (power * sin(rad) - power * cos(rad)) - rot * ratio;
+    int8_t power_1 = (power * sin(rad) + power * cos(rad)) + rot * ratio;
+    int8_t power_2 = (power * sin(rad) - power * cos(rad)) + rot * ratio;
+    int8_t power_3 = (power * sin(rad) + power * cos(rad)) - rot * ratio;
+
+    set_motors(power_0, power_1, power_2, power_3);
+}
--- zeus_car/zeus_car.ino
+++ zeus_car/zeus_car.ino
@@ -1,16 +1,6 @@
-// #include <Arduino.h>
-// #include <SoftPWM.h>
-// #include <string.h>
-
-/*******************************************************************
- * basic_move
- 
-  Control the direction and speed of motors rotation by pwm,
-  to make the car go forward, backward, left turn, right turn and stop.
-
-******************************************************************/
 #include <Arduino.h>
 #include <SoftPWM.h>
+#include <string.h>
 
 /*
  *  [0]--|||--[1]
@@ -20,125 +10,254 @@
  *   |         |
  *  [3]-------[2]
  */
+
 /** Set the pins for the motors */
 #define MOTOR_PINS \
-  (uint8_t[8]) { \
-    3, 4, 5, 6, A3, A2, A1, A0 \
-  }
+    (uint8_t[8]) { \
+        3, 4, 5, 6, A3, A2, A1, A0 \
+    }
+
 /** Set the positive and negative directions for the motors */
 #define MOTOR_DIRECTIONS \
-  (uint8_t[4]) { \
-    1, 0, 0, 1 \
-  }
+    (uint8_t[4]) { \
+        1, 0, 0, 1 \
+    }
 
-#define MOTOR_POWER_MIN 28  // 28/255
+#define MOTOR_POWER_MIN 28  // TODO: double check
+#define MOTOR_POWER_MAX 255 // TODO: double check
 
-int8_t power = 80;
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Zeus Car basic move");
-  SoftPWMBegin();  //init softpwm, before the motors initialization
-  carBegin();      // init motors
-}
-
-void loop() {
-  // carForward(power);
-  // delay(1000);
-  // carBackward(power);
-  // delay(1000);
-  // carLeft(power);
-  // delay(1000);
-  // carRight(power);
-  // delay(1000);
-  // carLeftForward(power);
-  // delay(1000);
-  // carLeftBackward(power);
-  // delay(1000);
-  // carRightForward(power);
-  // delay(1000);
-  // carRightBackward(power);
-  // delay(1000);
-  // carTurnLeft(power);
-  // delay(1000);
-  // carTurnRight(power);
-  // delay(1000);
-  // carStop();
-  // delay(2000);
-}
 /** Configure Wifi mode, SSID, password*/
 #define WIFI_MODE WIFI_MODE_AP
 #define SSID "Zeus_Car"
 #define PASSWORD "12345678"
 
+// Globals
+Esp32Listener esp_listener = Esp32Listener();
 
-void carBegin() {
-  for (uint8_t i = 0; i < 8; i++) {
-    SoftPWMSet(MOTOR_PINS[i], 0);
-    SoftPWMSetFadeTime(MOTOR_PINS[i], 100, 100);
-  }
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Initializing zeus_car");
+    SoftPWMBegin();  
+    rgb_begin();
+    rgb_write(ORANGE);
+    car_begin();      
+    esp_listener.begin(SSID, PASSWORD, WIFI_MODE, PORT);
+    esp_listener.set_on_receive(on_receive);
 }
 
-void carSetMotors(int8_t power0, int8_t power1, int8_t power2, int8_t power3) {
-  bool dir[4];
-  int8_t power[4] = { power0, power1, power2, power3 };
-  int8_t newPower[4];
+// Program loop
+void loop() {
+    
+    mode, action = esp_listener.listen();
 
-  for (uint8_t i = 0; i < 4; i++) {
-    dir[i] = power[i] > 0;
+    switch (mode) {
 
-    if (MOTOR_DIRECTIONS[i]) dir[i] = !dir[i];
+        case STANDBY:
+            rgb_write(PURPLE);
+            car_stop();
+            delay(1000);
+            break;
 
-    if (power[i] == 0) {
-      newPower[i] = 0;
-    } else {
-      newPower[i] = map(abs(power[i]), 0, 100, MOTOR_POWER_MIN, 255);
+        case ACT:
+            rgb_write(GREEN);
+            car_move(action);
+            break;
     }
-    SoftPWMSet(MOTOR_PINS[i * 2], dir[i] * newPower[i]);
-    SoftPWMSet(MOTOR_PINS[i * 2 + 1], !dir[i] * newPower[i]);
+}
+
+
+void start_car() {
+    for (uint8_t i = 0; i < 8; i++) {
+        SoftPWMSet(MOTOR_PINS[i], 0);
+        SoftPWMSetFadeTime(MOTOR_PINS[i], 100, 100);
+    }
+}
+
+void set_motors(int8_t power0, int8_t power1, int8_t power2, int8_t power3) {
+    bool dir[4];
+    int8_t power[4] = { power0, power1, power2, power3 };
+    int8_t newPower[4];
+
+    for (uint8_t i = 0; i < 4; i++) {
+        dir[i] = power[i] > 0;
+
+        // TODO: why invert it?
+        if (MOTOR_DIRECTIONS[i]) { 
+            dir[i] = !dir[i];
+        }
+
+        if (power[i] == 0) {
+            newPower[i] = 0;
+        } else {
+            newPower[i] = map(abs(power[i]), 0, 100, MOTOR_POWER_MIN, MOTOR_POWER_MAX);
+        }
+        SoftPWMSet(MOTOR_PINS[i * 2], dir[i] * newPower[i]);
+        SoftPWMSet(MOTOR_PINS[i * 2 + 1], !dir[i] * newPower[i]);
+    }
+}
+
+// TODO: make standby functionality
+void standby() {
+
+} 
+
+// TODO: make action functionality
+void act() {
+    rgbWrite(MODE_APP_CONTROL_COLOR);
+    carMoveFieldCentric(remoteAngle, remotePower, remoteHeading, appRemoteDriftEnable);
+    lastRemotePower = remotePower;
+} 
+
+// websocket received data processing
+void on_receive() {
+  // Serial.print("recv:");Serial.println(aiCam.recvBuffer);
+  irOrAppFlag = true;
+
+  // Mode select: line track without magnetic field, line track withmagnetic field, obstacle following, obstacle avoidance
+  current_button_state[0] = aiCam.getSwitch(REGION_M);
+  current_button_state[1] = aiCam.getSwitch(REGION_N);
+  current_button_state[2] = aiCam.getSwitch(REGION_O);
+  current_button_state[3] = aiCam.getSwitch(REGION_P);
+
+  // check change
+  bool is_change = false;
+  for(int i = 0; i < 4; i++) {
+    if(current_button_state[i] != last_button_state[i]) {
+      is_change = true;
+      last_button_state[i] = current_button_state[i];
+    }
   }
-}
+  // changed
+  if (is_change || currentMode == MODE_APP_CONTROL) {
+    if (current_button_state[0]) {
+        if(currentMode != MODE_LINE_TRACK_WITHOUT_MAG) {
+          carResetHeading();
+          currentMode = MODE_LINE_TRACK_WITHOUT_MAG;
+        }
+    } else if (current_button_state[1]) {
+      if(currentMode != MODE_LINE_TRACK_WITH_MAG) {
+        carResetHeading();
+        currentMode = MODE_LINE_TRACK_WITH_MAG;
+      }
+    } else if (current_button_state[2]) {
+      if(currentMode != MODE_OBSTACLE_FOLLOWING) {
+        carResetHeading();
+        currentMode = MODE_OBSTACLE_FOLLOWING;
+      }
+    } else if (current_button_state[3]) {
+      if(currentMode != MODE_OBSTACLE_AVOIDANCE) {
+        carResetHeading();
+        currentMode = MODE_OBSTACLE_AVOIDANCE;
+      }
+    } else {
+      if(currentMode != MODE_APP_CONTROL) {
+        appRemoteHeading = 0;
+        currentMode = MODE_NONE;
+      }
+    }
+  }
 
-void carForward(int8_t power) {
-  carSetMotors(power, power, power, power);
-}
+  // Stop
+  if (aiCam.getButton(REGION_F)) {
+    currentMode = MODE_NONE;
+    stop();
+    return;
+  }
 
-void carBackward(int8_t power) {
-  carSetMotors(-power, -power, -power, -power);
-}
+  // Compass Calibrate
+  if (aiCam.getButton(REGION_E)) {
+    currentMode = MODE_COMPASS_CALIBRATION;
+    carMove(0, 0, CAR_CALIBRATION_POWER); // rote to calibrate
+    compassCalibrateStart();
+    return;
+  }
 
-void carLeft(int8_t power) {
-  carSetMotors(-power, power, -power, power);
-}
+  // Reset Origin
+  if (aiCam.getButton(REGION_G)) {
+    currentMode = MODE_APP_CONTROL;
+    carStop();
+    carResetHeading();
+    appRemoteHeading = 0;
+    remoteHeading = 0;
+    return;
+  }
 
-void carRight(int8_t power) {
-  carSetMotors(power, -power, power, -power);
-}
+  //Joystick
+  uint16_t angle = aiCam.getJoystick(REGION_K, JOYSTICK_ANGLE);
+  uint8_t power = aiCam.getJoystick(REGION_K, JOYSTICK_RADIUS);
+  // power = map(power, 0, 100, 0, CAR_DEFAULT_POWER);
 
-void carLeftForward(int8_t power) {
-  carSetMotors(0, power, 0, power);
-}
+  if ( appRemoteAngle != angle || appRemotePower != power || angle != 0 || power != 0) {
+    if (currentMode != MODE_APP_CONTROL) {
+      currentMode = MODE_APP_CONTROL;
+      carResetHeading();
+    }
+    appRemoteAngle = angle;
+    remoteAngle = appRemoteAngle;
+    appRemotePower = power;
+    remotePower = appRemotePower;
+    appRemoteHeading = 0;
+    remoteHeading = 0; // reset remoteHeading parameter, avoid "IR remote control" changed this value
+  }
 
-void carLeftBackward(int8_t power) {
-  carSetMotors(-power, 0, -power, 0);
-}
+  // Drift 
+  if (appRemoteDriftEnable != aiCam.getSwitch(REGION_J)) {
+    if (currentMode != MODE_APP_CONTROL) {
+      currentMode = MODE_APP_CONTROL;
+      carResetHeading();
+    }
+    appRemoteDriftEnable = !appRemoteDriftEnable;
+  }
 
-void carRightForward(int8_t power) {
-  carSetMotors(power, 0, power, 0);
-}
+  // MoveHead
+  int moveHeadingA = aiCam.getJoystick(REGION_Q, JOYSTICK_ANGLE);
+  int16_t moveHeadingR = aiCam.getJoystick(REGION_Q, JOYSTICK_RADIUS);
+  if (appRemoteHeading != 0 || appRemoteHeadingR != 0 || appRemoteHeading != moveHeadingA || appRemoteHeadingR !=  moveHeadingR){
+    if (currentMode != MODE_APP_CONTROL) {
+      currentMode = MODE_APP_CONTROL;
+      carResetHeading();
+    }
+    appRemoteAngle = angle;
+    remoteAngle = appRemoteAngle;
+    appRemotePower = power;
+    remotePower = appRemotePower;
+    appRemoteHeading = moveHeadingA;
+    appRemoteHeadingR = moveHeadingR;
+    remoteHeading = moveHeadingA;
+    if (appRemoteDriftEnable && moveHeadingR == 0) { // Drift mode
+      carResetHeading();
+      appRemoteHeading = 0;
+      remoteHeading = 0;
+    }
+  }
 
-void carRightBackward(int8_t power) {
-  carSetMotors(0, -power, 0, -power);
-}
+  // Speech control
+  char speech_buf_temp[20];
 
-void carTurnLeft(int8_t power) {
-  carSetMotors(-power, power, power, -power);
-}
+  aiCam.getSpeech(REGION_I, speech_buf_temp);
+  if (strlen(speech_buf_temp) > 0) {
+    if (aiCam.send_doc["I"].isNull() == false) {
+      bool _last_stat = aiCam.send_doc["I"].as<bool>();
+      if (_last_stat == 1) {
+        aiCam.send_doc["I"] = 0;
+      } else {
+        aiCam.send_doc["I"] = 1;
+      }
+    } else {
+      aiCam.send_doc["I"] = 0;
+    }
+  } 
 
-void carTurnRight(int8_t power) {
-  carSetMotors(power, -power, -power, power);
-}
+  if (strcmp(speech_buf_temp, speech_buf) != 0) {
+    strcpy(speech_buf, speech_buf_temp);
+    if (strlen(speech_buf) > 0) {
+      int8_t cmd_code = text_2_cmd_code(speech_buf);
+      if (cmd_code != -1) {
+        remotePower = SPEECH_REMOTE_POWER;
+        currentMode = MODE_APP_CONTROL;
+        cmd_fuc_table[cmd_code]();
+      }
+    }
+  }
 
-void carStop() {
-  carSetMotors(0, 0, 0, 0);
 }
