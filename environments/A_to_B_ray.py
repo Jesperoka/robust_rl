342c910c0bb5f14575ce94a2bdcc39b0bfed8ffa
Jesperoka <jesperoka@gmail.com>
2024-03-19 22:02:11 +0100

    rllib has some issues when it comes to end-to-end GPU workflows, so I will make an attempt at pivoting to JaxMARL which seems to be what I was looking for all along when I switched from Brax to rllib.

--- environments/A_to_B_ray.py
+++ environments/A_to_B_ray.py
@@ -6,7 +6,7 @@ from sys import getsizeof
 
 from math import pi
 from gymnasium.spaces import Box, Space
-from typing import Callable, Optional, Tuple, Union, Set, List, Dict, Literal
+from typing import Any, Callable, Optional, Tuple, Union, Set, List, Dict, Literal
 from numpy import float32
 from functools import partial
 
@@ -122,8 +122,7 @@ class A_to_B(BaseEnv):
         self.reward:        tf.Tensor = tf.zeros((self.num_envs, 2), dtype=tf.float32)
         self.terminated:    tf.Tensor = tf.zeros((self.num_envs, ), dtype=tf.bool)
         self.truncated:     tf.Tensor = tf.zeros((self.num_envs, ), dtype=tf.bool)
-        self.info:          tf.Tensor = tf.zeros((self.num_envs, ), dtype=tf.float32)
-        pdb.set_trace()
+        self.info:          dict = {i: {key: {} for key in self.agent_ids} for i in range(self.num_envs)}                       # type: ignore[assignment]
 
         assert self.observation_min.shape == self.observation_max.shape, f"self.observation_min.shape = {self.observation_min.shape} should match self.observation_max.shape = {self.observation_max.shape}."   # type: ignore[attr-defined]
         assert self.observation_min.shape == self.observation[0].shape, f"self.observation_min.shape = {self.observation_min.shape} should match self.observation[0].shape = {self.observation[0].shape}."      # type: ignore[attr-defined]
@@ -158,17 +157,16 @@ class A_to_B(BaseEnv):
 
         # NOTE: I might need to add "__all__" as  special agent id to all returned elements
         # WARNING: do I need to mask out the terminated/truncated environments?
-        # TODO: discretize reward for ball proximity, and use sparse rewards while always resetting all envs at the same time
-    
-        return (
-                {i: {self.agent_ids[0]: row,    self.agent_ids[1]: row}      for i, row in enumerate(tf.unstack(self.observation, num=self.num_envs, axis=0))}, # type: ignore[assignment]
-                {i: {self.agent_ids[0]: row[0], self.agent_ids[1]: row[1]}   for i, row in enumerate(tf.unstack(self.reward,      num=self.num_envs, axis=0))}, # type: ignore[assignment]
-                {i: {self.agent_ids[0]: row,    self.agent_ids[1]: row}      for i, row in enumerate(tf.unstack(self.terminated,  num=self.num_envs, axis=0))}, # type: ignore[assignment]
-                {i: {self.agent_ids[0]: row,    self.agent_ids[1]: row}      for i, row in enumerate(tf.unstack(self.truncated,   num=self.num_envs, axis=0))}, # type: ignore[assignment]
-                {i: {self.agent_ids[0]: row,    self.agent_ids[1]: row}      for i, row in enumerate(tf.unstack(self.info,        num=self.num_envs, axis=0))}, # type: ignore[assignment]
-                {}
-                )
 
+        return (
+                {i: {key: row for key in self.agent_ids} for i, row in enumerate(tf.unstack(self.observation, num=self.num_envs, axis=0))},                     # type: ignore[assignment]
+                {i: {key: float(row[j]) for j, key in enumerate(self.agent_ids)} for i, row in enumerate(tf.unstack(self.reward, num=self.num_envs, axis=0))},  # type: ignore[assignment]
+                {i: {key: bool(row) for key in self.agent_ids} for i, row in enumerate(tf.unstack(self.terminated, num=self.num_envs, axis=0))},                # type: ignore[assignment]
+                {i: {key: bool(row) for key in self.agent_ids} for i, row in enumerate(tf.unstack(self.truncated, num=self.num_envs, axis=0))},                 # type: ignore[assignment]
+                self.info,
+                {}
+
+                )
     @override(BaseEnv)
     def send_actions(self, action_dict: MultiEnvDict) -> None:
 
@@ -176,50 +174,51 @@ class A_to_B(BaseEnv):
         def action_dict_to_tensor(action_dict: MultiEnvDict) -> tf.Tensor:
             return tf.concat([
                 tf.concat([
-                    tf.expand_dims(agent_dict[self.agent_ids[0]], axis=0), 
-                    tf.expand_dims(agent_dict[self.agent_ids[1]], axis=0)
+                    tf.expand_dims(tensor, axis=0) for tensor in agent_dict.values()
                     ], axis=1) for agent_dict in action_dict.values()
-                ], axis=0)                                                                                         # type: ignore[assignment]
+                ], axis=0)                                                                                          # type: ignore[assignment]
 
         action: tf.Tensor = action_dict_to_tensor(action_dict)                                                      # type: ignore[attr-defined]
 
+        # TODO: discretize reward for ball proximity, and use sparse rewards while always resetting all envs at the same time
         self.observation, self.reward, self.terminated, self.truncated, self.info = self.step(action)
         self.should_return_empty = False
 
     @override(BaseEnv)
     def try_reset( self, env_id: Optional[EnvID] = None, *, seed: Optional[int] = None, options: Optional[dict] = None) -> Tuple[Optional[MultiEnvDict], Optional[MultiEnvDict]]:
-        self.prng_key, reset_rng = utils.vmapped_tf_random_split(self.prng_key, 2)                                   # type: ignore[attr-defined]
+        self.prng_key, reset_rng = utils.vmapped_tf_random_split(self.prng_key, 2)                                  # type: ignore[attr-defined]
         assert self.prng_key.shape == (self.num_envs, 2), f"self.prng_key.shape = {self.prng_key.shape} should be equal to (self.num_envs, 2) = ({self.num_envs}, 2)."
         assert reset_rng.shape == (self.num_envs, 2), f"reset_rng.shape = {reset_rng.shape} should be equal to (self.num_envs, 2) = ({self.num_envs}, 2)."
         
         # TODO: make individual env reset masking 
-        self.observation = self.reset(reset_rng)                                                                    # type: ignore[attr-defined]
-        observation: MultiEnvDict = {i: {self.agent_ids[0]: row, self.agent_ids[1]: row} for i, row in enumerate(tf.unstack(self.observation, num=self.num_envs, axis=0))} # type: ignore[assignment]
+        self.observation: tf.Tensor = self.reset(reset_rng)                                                         # type: ignore[attr-defined]
+        observation: MultiEnvDict = {i: {key: row for key in self.agent_ids} for i, row in enumerate(tf.unstack(self.observation, num=self.num_envs, axis=0))}  # type: ignore[assignment]
         
-        return observation, {} 
-
-    # TODO: figure out which actually need to be implemented
+        return observation, self.info
 
     @override(BaseEnv)
-    def try_restart(self, env_id: Optional[EnvID] = None) -> None: 
-        raise NotImplementedError
+    def try_restart(self, env_id: Optional[EnvID] = None) -> None: raise NotImplementedError
 
     @override(BaseEnv)
     def get_sub_environments(self, as_dict: bool = False) -> Union[Dict[int, BaseEnv], List[BaseEnv]]: 
         env = BaseEnv() # supply unimplemented BaseEnv to alert if anyone uses anything other than the dict keys.
         return {i: env for i in range(self.num_envs)} if as_dict else [env]*self.num_envs
 
+    # TODO: implement
     @override(BaseEnv)
-    def try_render(self, env_id: Optional[EnvID] = None) -> None: 
-        raise NotImplementedError
+    def try_render(self, env_id: Optional[EnvID] = None) -> None: raise NotImplementedError
 
     @property
-    @override(BaseEnv) # NOTE: check_env never directly uses action_space for BaseEnv
-    def action_space(self) -> Space[Box]: raise NotImplementedError                                                 # type: ignore[override]
+    @override(BaseEnv) 
+    def action_space(self) -> Space[Box]:
+        with tf.init_scope():
+            return Box(self.action_min.numpy(), self.action_max.numpy(), dtype=float32)                                 # type: ignore[override]
 
     @property
-    @override(BaseEnv) # NOTE: check_env never directly uses observation_space for BaseEnv
-    def observation_space(self) -> Space[Box]: raise NotImplementedError
+    @override(BaseEnv)
+    def observation_space(self) -> Space[Box]: 
+        with tf.init_scope():
+            return Box(self.observation_min.numpy(), self.observation_max.numpy(), dtype=float32)                       # type: ignore[override]
 
     @override(BaseEnv)
     def observation_space_contains(self, x: MultiEnvDict) -> bool:
@@ -257,8 +256,7 @@ class A_to_B(BaseEnv):
             } for i in range(self.num_envs)}
 
     @override(BaseEnv)
-    def get_agent_ids(self) -> Set[AgentID]: 
-        return {"Zeus", "Panda"}
+    def get_agent_ids(self) -> Set[AgentID]: return set(self.agent_ids)
 
     # -------------------------------------------------------------------------------------------
     # ----------------------------------- end rllib overrides -----------------------------------
@@ -286,7 +284,7 @@ class A_to_B(BaseEnv):
         observation: tf.Tensor = tf.concat((qpos, qvel, p_goal), axis=1)                                            # type: ignore[attr-defined]
         assert observation.shape == (self.num_envs, *self.observation_min.shape), f"observation.shape = {observation.shape} should be equal to (self.num_envs, *self.observation_min.shape) = ({self.num_envs}, *{self.observation_min.shape})."  # type: ignore[attr-defined]
     
-        return observation 
+        return observation
 
     @tf.function(jit_compile=True)
     def reset_car_arm_and_gripper(self, rng: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
@@ -336,7 +334,7 @@ class A_to_B(BaseEnv):
 
     # --------------------------------------- begin step ---------------------------------------- 
     # -------------------------------------------------------------------------------------------
-    def step(self, action: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+    def step(self, action: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, dict]:
         ctrl: tf.Tensor = self.compute_controls(action)
         self.mjx_data: Data = self.vmapped_step(self.mjx_model, self.mjx_data, ctrl)
         observation, reward, terminated, truncated, info = self.evaluate_environment(self.observe(), action)        # type: ignore[attr-defined] 
@@ -371,7 +369,7 @@ class A_to_B(BaseEnv):
         return ctrl
 
     tf.function(jit_compile=True)
-    def evaluate_environment(self, observation, action) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+    def evaluate_environment(self, observation, action) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor, dict]:
         q_car: tf.Tensor; q_arm: tf.Tensor; q_gripper: tf.Tensor; q_ball: tf.Tensor; qd_car: tf.Tensor; qd_arm: tf.Tensor; qd_gripper: tf.Tensor; qd_ball: tf.Tensor; p_goal: tf.Tensor
 
         (q_car, q_arm, q_gripper, 
@@ -390,26 +388,23 @@ class A_to_B(BaseEnv):
         arm_outside_limits_reward:  tf.Tensor = 100.0*tf.cast(arm_outside_limits, tf.float32)                       # type: ignore[assignment]
         zeus_reward, panda_reward = tf.vectorized_map(self.jitted_reward_function, (observation, action))           # type: ignore[assignment]
 
-        pdb.set_trace()
         zeus_reward:    tf.Tensor = zeus_reward + car_goal_reward - arm_goal_reward - car_outside_limits_reward     # type: ignore[assignment]
         panda_reward:   tf.Tensor = panda_reward + arm_goal_reward - car_goal_reward - arm_outside_limits_reward    # type: ignore[assignment]
-        reward:         tf.Tensor = tf.stack((zeus_reward, panda_reward), axis=1)                                  # type: ignore[assignment]
+        reward:         tf.Tensor = tf.stack((zeus_reward, panda_reward), axis=1)                                   # type: ignore[assignment]
 
         terminated: tf.Tensor = tf.logical_or(car_goal_reached, arm_goal_reached)
 
         # NOTE: need to use "__all__"
         # TODO: create info or do other logging
         truncated:  tf.Tensor = tf.zeros((self.num_envs, ), dtype=tf.bool)                                            
-        info:       tf.Tensor = tf.zeros((self.num_envs, ), dtype=tf.float32)
 
-        return observation, reward, terminated, truncated, info
+        return observation, reward, terminated, truncated, {} 
     # ---------------------------------------- end step ----------------------------------------- 
     # -------------------------------------------------------------------------------------------
     
 
     # ------------------------------------ begin subroutines ------------------------------------
     # -------------------------------------------------------------------------------------------
-    # BUG: rllib expects reward function to return python int or float
     @utils.method_takes_one_argument
     def jitted_reward_function(self, observation: tf.Tensor, action: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
         @tf.function(jit_compile=True, input_signature=(tf.TensorSpec(shape=observation.shape, dtype=tf.float32), tf.TensorSpec(shape=action.shape)))
@@ -417,14 +412,13 @@ class A_to_B(BaseEnv):
             return self.reward_function(observation, action)
         return reward(observation, action)                                                                          # type: ignore[assignment]
 
-
     def get_car_orientation(self) -> tf.Tensor:
         return self.mjx_data.qpos[:, self.car_orientation_index]                                                    # type: ignore[assingment]
     
     @tf.function(jit_compile=True)
     def observe(self) -> tf.Tensor: # NOTE: keeping for future flexibility
         return self.observation
-        # return tf.concat([                                                                                          # type: ignore[assingment]
+        # return tf.concat([                                                                                        # type: ignore[assingment]
         #     self.mjx_data.qpos,
         #     self.mjx_data.qvel,
         #     self.p_goal
@@ -454,7 +448,7 @@ class A_to_B(BaseEnv):
 
     # BUG: I might not need to do this myself, I suspect rllib does it for me based on observation_space
     def scale_action(self, tanh_action: tf.Tensor, minval: tf.Tensor, maxval: tf.Tensor) -> tf.Tensor:
-        return 0.5*(maxval - minval)*tanh_action + 0.5*(maxval + minval)                                    # type: ignore[attr-defined]
+        return 0.5*(maxval - minval)*tanh_action + 0.5*(maxval + minval)                                            # type: ignore[attr-defined]
 
     def reset_car(self, rng_car: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
         # assert False, f"{type(rng_car)}, {rng_car.shape}, {rng_car.dtype}"
@@ -522,8 +516,9 @@ class A_to_B(BaseEnv):
             return tf.constant([0.02, -0.025, 0.02, -0.025], dtype=tf.float32)                                      # type: ignore[assignment] 
         def release() -> tf.Tensor: 
             return tf.constant([0.04, 0.05, 0.04, 0.05], dtype=tf.float32)                                          # type: ignore[assignment] 
+    
+        return float(action[0] >= 0.0)*grip() + float(action[0] < 0.0)*release()                                    # type: ignore[attr-defined]
 
-        return tf.cond(action[0] > 0.0, grip, release)                                                              # type: ignore[attr-defined]
 
     
 
@@ -552,7 +547,7 @@ if __name__ == "__main__":
     from os import environ
     environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform" # want jax to deallocate GPU memory after use
 
-    from ray.tune import Tuner
+    from ray.tune import Tuner, run 
     from ray.air import RunConfig
     from ray.tune.registry import register_env
     from ray.rllib.algorithms.ppo import PPOConfig
@@ -569,7 +564,6 @@ if __name__ == "__main__":
 
 
     SCENE = "mujoco_models/scene.xml"
-
 
 
     model: MjModel = MjModel.from_xml_path(SCENE)                                                                      
@@ -607,19 +601,43 @@ if __name__ == "__main__":
     ngpus = 1
     ncpus = 8
 
+    # NOTE: Considering overriding the SampleCollector class to implement end-to-end GPU training. Will not do this before everything else is working.
+
     config = (
             PPOConfig()
+            .experimental(
+                _enable_new_api_stack=False,
+                _disable_preprocessor_api=True,
+                _disable_initialize_loss_from_dummy_batch=True,
+                )
             .environment(A_to_B.__name__)
-            .experimental(_enable_new_api_stack=True)
-            .rollouts(num_rollout_workers=0, num_envs_per_worker=num_envs)
             .framework("tf2")
+            .rollouts(
+                num_rollout_workers=0, 
+                num_envs_per_worker=num_envs,
+                batch_mode="complete_episodes",
+                preprocessor_pref="deepmind",
+                )
             .multi_agent(
                 policies = {
-                    "Zeus": PolicySpec(action_space=zeus_action_space, config=PPOConfig.overrides(framework_str="tf2")),
-                    "Panda": PolicySpec(action_space=panda_action_space, config=PPOConfig.overrides(framework_str="tf2"))
+                    "Zeus": PolicySpec(
+                        action_space=zeus_action_space, 
+                        config=PPOConfig()
+                        .experimental(_enable_new_api_stack=False)
+                        .framework("tf2")
+                        .training(lr=0.001)
+                        ),
+                    "Panda": PolicySpec(
+                        action_space=panda_action_space, 
+                        config=PPOConfig()
+                        .experimental(_enable_new_api_stack=False)
+                        .framework("tf2")
+                        .training(lr=0.001)
+                        )
                     },
                 policy_mapping_fn = lambda agent_id, *args, **kwargs: agent_id,
-                policies_to_train = ["Zeus", "Panda"]
+                policies_to_train = ["Zeus", "Panda"],
+                count_steps_by = "env_steps"
                 )
             .resources(
                 num_gpus=ngpus, 
@@ -629,14 +647,6 @@ if __name__ == "__main__":
                 num_cpus_per_worker=ncpus,
                 num_cpus_for_local_worker=ncpus,
                 )
-            .rl_module(
-                rl_module_spec=MultiAgentRLModuleSpec(
-                    module_specs={
-                        "Zeus": SingleAgentRLModuleSpec(),
-                        "Panda": SingleAgentRLModuleSpec()
-                        }
-                    )
-                )
             )
     
     stop = {
@@ -645,9 +655,16 @@ if __name__ == "__main__":
             "timesteps_total": 1000
             }
 
-    check_env(A_to_B(mjx_model, mjx_data, grip_site_id, options), config)
+    # check_env(A_to_B(mjx_model, mjx_data, grip_site_id, options), config)
+    # input("wait")
+    
+    pdb.set_trace()
+    algo = config.build()
 
-    input("wait")
+    for i in range(10):
+        print(algo.train())
+
+    # input("wait")
 
     init(num_gpus=1, num_cpus=8) 
     results = Tuner(
